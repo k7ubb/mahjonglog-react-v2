0f7c76b7bc765234e76d5d48eddd1a79b10ccf20
k7ubb <kubota.sou@vega.ocn.ne.jp>
2024-11-17 15:28:10 +0900

    IndexのstyleをModule cssに変更

--- src/components/Pages/IndexPage.tsx
+++ src/components/Pages/IndexPage.tsx
@@ -1,18 +1,18 @@
 import { Link } from 'react-router-dom';
-import '../Presenter/IndexPage.css';
+import style from './IndexPage.module.css';
 
 export const IndexPage: React.FC = () => {
 	return (
-		<>
+		<div className={style.indexPage}>
 			<header>
-				<Link to="/" className="sitename">
+				<Link to="/" className={style.sitename}>
 					<img src="/icon.png" alt="sitelogo" />
 					麻雀戦績共有アプリ
 				</Link>
 			</header>
 			<main>
 				<p>
-					<Link to="/app" className="download_button">
+					<Link to="/app" className={style.download_button}>
 						<i className="fa-solid fa-map"></i>アプリを開く
 					</Link>
 				</p>
@@ -20,6 +20,6 @@ export const IndexPage: React.FC = () => {
 			<footer>
 				© 2023 <a href="https://bb.xrea.jp/">bb.xrea.jp</a>
 			</footer>
-		</>
+		</div>
 	);
 };
